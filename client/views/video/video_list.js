d426579cdbbd955987d7ee708d97eca779d31547
Connor Taylor <conbond93@gmail.com>
2014-04-22 17:57:37 -0400

    updated layout

---
+++ client/views/video/video_list.js
@@ -0,0 +1,38 @@
+var videoData = [
+	{
+		title:"Chris Penny's Homepage",
+		url:"www.fag.com"
+	},
+	{
+		title:"Intrinsic Audio",
+		url:"www.intrinsic-audio.com"
+	},
+	{
+		title:"Chris Penny's Homepage",
+		url:"www.fag.com"
+	},
+	{
+		title:"Chris Penny's Homepage",
+		url:"www.fag.com"
+	},
+	{
+		title:"Chris Penny's Homepage",
+		url:"www.fag.com"
+	},
+	{
+		title:"Chris Penny's Homepage",
+		url:"www.fag.com"
+	},
+	{
+		title:"Chris Penny's Homepage",
+		url:"www.fag.com"
+	},
+	{
+		title:"Chris Penny's Homepage",
+		url:"www.fag.com"
+	}
+];
+
+Template.videoList.helpers({
+	videos: videoData
+});
